51aaa0177022c3562b89827e7fdb9b16c02a118b
redstarnight <redstarnight1@gmail.com>
2022-01-15 22:19:43 +0330

    testttt branch

--- fileinTestBranch.php
+++ fileinTestBranch.php
@@ -1,5 +1,16 @@
 <?php
 
     echo "efefefef";
+    echo "
+        git add "file name"
+        git commit
+        
+        git checkout "Branch name"
+        git pull 
+        git push
+        git merge 
+        git status
+    
+    ";
 
 ?>
