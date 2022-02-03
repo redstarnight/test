3feaf88509736f4e72fac0f969ec483c76c26a86
redstarnight <redstarnight1@gmail.com>
2022-02-03 21:20:32 +0330

    3.php

---
+++ 3.php
@@ -0,0 +1,32 @@
+<?php
+
+    $a=20;
+    if($a<=10){
+            return false;
+    }else{
+        echo "
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, magnam. Voluptas recusandae, facilis earum iste fuga minus saepe sapiente? Fuga voluptatum ab laboriosam laborum. Ullam similique recusandae eum deleniti molestias?
+            Molestias, omnis doloribus! Atque, vel magnam! In adipisci dolorem debitis, sunt quisquam officiis saepe beatae, nostrum excepturi quidem incidunt voluptatibus consequatur voluptatem? Nam aut quam est harum inventore. Iure, voluptate?
+            Praesentium recusandae ipsa placeat harum commodi similique laborum? Quisquam perferendis laborum, maxime blanditiis praesentium a sapiente iure fugiat rerum corporis fugit molestiae animi aliquid explicabo. At officiis eos quam animi.
+            Assumenda rem pariatur rerum, aspernatur quasi blanditiis, qui libero sit ullam iusto quaerat. Quibusdam doloribus, excepturi provident dolorem ratione expedita perspiciatis vero. Ex itaque aut aperiam placeat, delectus atque vel.
+            Dicta doloremque nihil esse dignissimos explicabo quis nisi, facere veritatis rerum nam vel maiores perspiciatis impedit nobis adipisci at quod odio eaque ratione blanditiis obcaecati. Ex in animi eos rem.
+            Delectus vitae adipisci possimus exercitationem quidem voluptates harum similique veniam enim sunt tempore ipsum nobis praesentium iure magni maiores eaque, dignissimos cumque distinctio commodi repellat alias. Dolor at explicabo tenetur!
+            Iusto, assumenda facere! Odit, numquam tenetur atque ab aliquam ut ratione id pariatur, optio autem eius officia soluta. Ex labore vitae veritatis quis corporis at itaque est sed voluptatibus laboriosam!
+            Quo quidem illo repellat omnis natus voluptate ipsam tempora a ex. Perspiciatis nam autem ipsum, laudantium error neque in itaque sint esse sapiente corporis ipsam labore quos fuga soluta at!
+            Excepturi corrupti nihil sunt ipsum est error, veniam officiis laudantium voluptate odit necessitatibus nemo. Pariatur consequatur placeat magni repudiandae eos natus nesciunt accusamus in voluptatum reprehenderit cum quod, quam iste!
+            Voluptas aliquid totam in minus praesentium fugiat autem aspernatur non eos! Culpa laboriosam, ratione aut provident harum labore architecto earum maxime ipsum natus, dolor doloribus adipisci sunt voluptatum qui impedit?
+            Ad, odio. Pariatur quasi, sunt odit sit ab tempore? Aut impedit, nostrum officiis quibusdam cupiditate doloremque nam voluptatem explicabo ut consequatur adipisci delectus incidunt assumenda dolorem totam eos cumque! Atque.
+            Suscipit, amet voluptatem quasi officiis modi dicta doloremque sapiente quaerat omnis deserunt quae non recusandae aperiam molestiae nostrum quo praesentium. Placeat harum consequuntur in tempore corporis fugiat, alias sequi tempora.
+            Id adipisci mollitia ipsam nulla cupiditate, eos magni saepe alias labore amet aperiam possimus voluptates quasi tempora voluptatibus fuga minima reprehenderit pariatur nisi maxime numquam. Placeat dolores quos maxime aut.
+            Ad dolorum labore voluptas ut cupiditate quis dignissimos autem et fuga magni quidem optio veniam, temporibus tempore laudantium. Consectetur est optio suscipit et explicabo deserunt praesentium. Autem, cum sit! Officiis.
+            Illum explicabo hic itaque repellendus fugiat accusamus ipsam ducimus nobis dolor, quae provident numquam doloremque corporis optio ipsa at maiores necessitatibus, voluptates similique asperiores iste dignissimos sit. Molestiae, obcaecati inventore?
+            Voluptates id adipisci eius dolorem? Aliquid, harum vel qui enim fuga nihil, omnis cupiditate et a corrupti aut ipsam perferendis dolor quasi reprehenderit earum fugit! Totam, eius doloribus. Fugiat, in!
+            Corrupti eligendi cum placeat porro ducimus voluptates hic, incidunt quis dolores? Dolorem, sequi. Saepe excepturi, id iure repellat voluptas ut facere accusantium quasi delectus quae architecto deserunt blanditiis quam corporis.
+            Mollitia corrupti illum possimus quaerat! Doloribus laudantium iste commodi, hic ullam deserunt iure delectus odio quod provident esse perferendis. Cupiditate quidem ducimus officiis quae. Expedita ratione in enim explicabo eius!
+            Quaerat qui commodi ad, voluptate, ut pariatur sint facilis inventore esse reiciendis dolorem excepturi. Perferendis est fugit veniam deleniti cum, quae quibusdam nostrum, ipsum totam unde necessitatibus. Ea, exercitationem. Eveniet.
+            Debitis harum quod quidem odit, enim quaerat rem ut nisi incidunt hic ad laborum dolorem officia nostrum quos excepturi dolore pariatur eaque quis id recusandae? Cum dolorem voluptas nisi vero.
+        ";
+        return true;
+    }
+
+?>
